d3985c3e3a8dc081b89675706037421fe9faf952
Davyd Holovii <mirage.present@gmail.com>
2023-08-30 21:11:53 -0400

    Validator v1

--- src/Validator.php
+++ src/Validator.php
@@ -3,8 +3,6 @@
 namespace MiragePresent\BattleshipValidator;
 
 class Validator {
-    protected array $validSpots = [];
-
     protected array $occupiedPoints = [];
 
     protected array $remainingShips = [
@@ -21,8 +19,14 @@ class Validator {
     public function isValid(): bool 
     {
         // processing field metadata
-        $this->createValidPoints();
         $this->loadPoints();
+
+        // The number of occupied spots must be exactly 20
+        // Otherwise the field is invalid
+        if (count($this->occupiedPoints) !== 20) {
+            return false;
+        }
+
         $this->linkPoints();
 
         // validate field
@@ -31,29 +35,19 @@ class Validator {
             && $this->validNumberOfShips();
     }
 
-    protected function createValidPoints(): void 
-    {
-        for ($x = 1; $x <= 10; $x++) {
-            for ($y = 1; $y <= 10; $y++) {
-                $this->validSpots[$this->pointKey($x, $y)] = true;
-            }
-        }
-    }
-
     protected function loadPoints(): void 
     {
-        $x = 1;
+        $y = 1;
         foreach ($this->field as $row) {
-            $y = 1;
+            $x = 1;
             foreach ($row as $cell) {
-                if ($cell === 1) {
+                if ($cell) {
                     $this->addOccupiedPoint($x, $y);
-                    $this->removeFromValidPoint($x, $y);
                 }
 
                 $x++;
             }
-            $x++;
+            $y++;
         }
     }
 
@@ -73,7 +67,7 @@ class Validator {
 
             if (isset($this->occupiedPoints[$nextY])) {
                 $this->occupiedPoints[$key]['nextY'] = $nextY;
-                $this->occupiedPoints[$nextX]['prevY'] = $key;
+                $this->occupiedPoints[$nextY]['prevY'] = $key;
             }
         }
     }
@@ -81,12 +75,18 @@ class Validator {
     protected function validSpots(): bool 
     {
         foreach ($this->occupiedPoints as $point) {
-            if (isset($point['nextX']) && isset($point['nextY'])) {
-                return false;
+            // No siblings on X axis which means 
+            // even if there are siblings on Y it's one axis ship
+            if (!isset($point['nextX']) && !isset($point['prevX'])) {
+                continue;
             }
-            if (isset($point['prevX']) && isset($point['prevY'])) {
-                return false;
+            // No siblings on X axis which means 
+            // even if there are siblings on Y it's one axis ship
+            if (!isset($point['nextY']) && !isset($point['prevY'])) {
+                continue;
             }
+            // There are siblings on X and Y axes
+            return false;
         }
 
         return true;
@@ -94,12 +94,63 @@ class Validator {
 
     protected function validPlacement(): bool 
     {
+        foreach ($this->occupiedPoints as $point) {
+            $x = $point['x'];
+            $y = $point['y'];
+
+            // Validate surrounding corners or a point are empty
+            $bottomLeftCorner = $this->pointKey($x - 1, $y + 1);
+            $bottomRightCorner = $this->pointKey($x + 1, $y + 1);
+
+            // Bottom left corner must be empty
+            if (isset($this->occupiedPoints[$bottomLeftCorner])) {
+                return false;
+            }
+
+            // Bottom right corner must be empty
+            if (isset($this->occupiedPoints[$bottomRightCorner])) {
+                return false;
+            }
+            // No need to check top corners they are covered 
+            // while checking points from top to the bottom
+        }
+
         return true;
     }
 
     protected function validNumberOfShips(): bool 
     {
+        $remainingSpots = $this->occupiedPoints;
+
+        while (!empty($remainingSpots)) {
+            // Take the first point from unporcessed
+            reset($remainingSpots);
+            $pointKey = key($remainingSpots);
+            $ship = $this->getShip($pointKey, $this->occupiedPoints);
+
+            $size = count($ship);
+
+            if ($size > 4 || $size === 0) {
+                return false;
+            }
+
+            $this->remainingShips[$size]--;
+
+            if ($this->remainingShips[$size] < 0) {
+                return false;
+            }
+
+            foreach ($ship as $key) {
+                unset($remainingSpots[$key]);
+            }
+        } 
+
         return true;
+    }
+
+    protected function pointKey(int $x, int $y): string 
+    {
+        return sprintf("%d-%d", $x, $y);
     }
 
     protected function addOccupiedPoint(int $x, int $y): void 
@@ -107,17 +158,37 @@ class Validator {
         $this->occupiedPoints[$this->pointKey($x, $y)] = compact(['x', 'y']);
     }
 
-    protected function removeFromValidPoint(int $x, int $y): void 
+    /**
+     * Retuns point keys that are linked into a ship 
+     * by one of the point of the ship
+     * 
+     * @param string $pointKey
+     * @param array $pointsOfField
+     * 
+     * @return array All the ship point keys
+     */
+    protected function getShip(string $pointKey, array $pointsOnField): array 
     {
-        $pointKey = $this->pointKey($x, $y);
-        
-        if (isset($this->validSpots[$pointKey])) {
-            unset($this->validSpots[$pointKey]);
+        $point = $pointsOnField[$pointKey];
+        // Make sure the point is not in the middle of a ship
+        if (!empty($point['prevX'])) {
+            return $this->getShip($point['prevX'], $pointsOnField);
+        } elseif (!empty($point['prevY'])) {
+            return $this->getShip($point['prevY'], $pointsOnField);
         }
-    }
 
-    protected function pointKey(int $x, int $y): string 
-    {
-        return sprintf("%d-%d", $x, $y);
+        $shipPoints = [];
+        $currentKey = $pointKey;
+
+        while(!empty($currentKey)) {
+            $currentPoint = $pointsOnField[$currentKey];
+            $shipPoints[] = $currentKey;
+
+            $currentKey = $currentPoint['nextX'] 
+                ?? $currentPoint['nextY'] 
+                ?? null;
+        }
+
+        return $shipPoints;
     }
 }
